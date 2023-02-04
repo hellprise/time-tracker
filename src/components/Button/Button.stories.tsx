a6048791f36461709bb05a939ddfeb1d1aef43b0
DethMaix <pavlukmaksim95@gmail.com>
2023-02-04 17:05:07 +0200

    first version of button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,56 +5,46 @@ import { Button } from "./Button";
 export default {
   title: "Button",
   component: Button,
-  // argTypes: {
-  //   color: {
-  //     type: "string",
-  //     description: "Варіанти кольору кнопки",
-  //     defaultValue: "primary",
-  //     control: {
-  //       type: "radio",
-  //       options: ["primary", "secondary", "transparent"],
-  //     },
-  //   },
-  //   size: {
-  //     type: "string",
-  //     description: "Варіанти розміру кнопки",
-  //     defaultValue: "s",
-  //     control: {
-  //       type: "radio",
-  //       options: ["none", "s", "m", "l", "xl"],
-  //     },
-  //   },
-  //   children: {
-  //     type: "string",
-  //     description: "Текст кнопки",
-  //     name: "label",
-  //     defaultValue: "It's a button!",
-  //   },
-  // },
+  argTypes: {
+    color: {
+      type: "string",
+      description: "Варіанти кольору кнопки",
+      defaultValue: "primary",
+      control: {
+        type: "radio",
+        options: ["primary", "secondary", "transparent"],
+      },
+    },
+    size: {
+      type: "string",
+      description: "Варіанти розміру кнопки",
+      defaultValue: "s",
+      control: {
+        type: "radio",
+        options: ["s", "m", "l"],
+      },
+    },
+    children: {
+      type: "string",
+      description: "Текст кнопки",
+      name: "label",
+      defaultValue: "It's a button!",
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  color: "primary",
-  children: "It's a primary!",
+export const notActive = Template.bind({});
+notActive.args = {
+  isActive: false,
+  children: "It's a not Active!",
   size: "s",
-  onClick: () => console.log("click"),
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  color: "secondary",
-  children: "It's a secondary!",
-  size: "m",
-  onClick: () => console.log("click"),
-};
-
-export const Transparent = Template.bind({});
-Transparent.args = {
-  color: "transparent",
-  children: "It's transparent!",
+export const Active = Template.bind({});
+Active.args = {
+  isActive: true,
+  children: "It's Active!",
   size: "l",
-  onClick: () => console.log("click"),
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,25 +10,28 @@ interface IButtonProps
   extends DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   size?: size;
   color?: color;
+  isActive?: boolean;
 }
 
 export const Button: FC<PropsWithChildren<IButtonProps>> = ({
   children,
   size = "s",
   color = "primary",
+  isActive = "false",
   className,
   ...props
 }) => {
   return (
     <button
       {...props}
-      className={clsx("", className, {
+      className={clsx([st.Button], className, {
         [st.s]: size === "s",
         [st.m]: size === "m",
         [st.l]: size === "l",
         [st.primary]: color === "primary",
         [st.secondary]: color === "secondary",
         [st.transparent]: color === "transparent",
+        [st.active]: isActive,
       })}
     >
       {children}
